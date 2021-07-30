1440efcceac0b4b7c651b355bd280945d878c380
Danil <68150308+mo0nchild@users.noreply.github.com>
2021-07-30 18:50:51 +0300

    Add files via upload

--- web/main.dart
+++ web/main.dart
@@ -6,5 +6,5 @@ import 'package:over_react/react_dom.dart' as react_dom;
 import 'package:over_react/over_react_redux.dart';
 import 'package:dart_application_1/app.dart';
 
-void main(List<String> args) =>
-    react_dom.render(MyWidget()(), querySelector('#root'));
+void main(List<String> args) => react_dom.render(
+    (MyWidget()..MainColor = 'crimson')(), querySelector('#root'));
